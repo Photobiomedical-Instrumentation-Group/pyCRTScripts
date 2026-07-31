97755c2d9b9468c5c1798e89a095d226efb97c0f
eduardotogpi <eduardotogpi@noreply.codeberg.org>
2026-07-31 19:35:20 +0300

    Added github workflow to publish exe files

--- measure_crt.py
+++ measure_crt.py
@@ -1,13 +1,15 @@
 from functions import multiVideoPipeline, singleVideoPipeline
-from gui import (askSingleVideoOrDirectory, showDirSuccessMessage,
-                 showVideoErrorMessage, showVideoSuccessMessage)
+from gui import (
+    askSingleVideoOrDirectory,
+    showDirSuccessMessage,
+    showVideoErrorMessage,
+    showVideoSuccessMessage,
+)
 
 answer = askSingleVideoOrDirectory()
 
 if answer == "directory":
-    failedMeasurements = multiVideoPipeline(
-        "configuration.toml", "defaults.toml"
-    )
+    failedMeasurements = multiVideoPipeline("configuration.toml", "defaults.toml")
     showDirSuccessMessage(failedMeasurements)
 else:
     if singleVideoPipeline("configuration.toml", "defaults.toml"):
